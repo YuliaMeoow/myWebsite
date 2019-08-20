52f8941b61949d0bbc52c2866e1fc31aa7f3575f
YuliaMeoow <ring.yuliya@gmail.com>
2019-08-20 14:30:53 +0300

    delete form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,39 +1,15 @@
 import React from 'react';
-import { Button, Form, FormGroup, Label, Input, Container, Col} from 'reactstrap';
 
 import './styles/contact.css'
 
 export default class Example extends React.Component {
     render() {
       return (
-          <Container className='contact-form' href='contacts'>
+          <div className='contact' href='contacts'>
               <h2>Свяжитесь со мной</h2> 
-            <Form>
-                <FormGroup row>
-                    <Label for='' sm={2} size='lg'></Label>
-                    <Col className='col-lg-8 col-md-8 col-sm-12'>
-                        <Input type='text' placeholder='Введите имя' bsSize='lg' />
-                    </Col>
-                </FormGroup>
-                <FormGroup row>
-                    <Label for='' sm={2} size='lg'></Label>
-                    <Col className='col-lg-8 col-md-8 col-sm-12'>
-                        <Input type='text' placeholder='Введите email' bsSize='lg' />
-                    </Col>
-                </FormGroup>
-                <FormGroup row>
-                    <Label for='' sm={2} size='lg'></Label>
-                    <Col className='col-lg-8 col-md-8 col-sm-12'>
-                        <Input type='textarea' bsSize='lg' />
-                    </Col>
-                </FormGroup>
-                <FormGroup check row>
-                    <Col sm={{ size: 10, offset: 8 }}>
-                        <Button className='btn btn-dark'>Отправить</Button>
-                    </Col>
-                </FormGroup>
-            </Form>
-        </Container>
+              <hr align="center" width="50" size="10" color="#4b4b4b" />
+              <h2>ring.yuliya@gmail.com</h2> 
+        </div>
       );
     }
   }
