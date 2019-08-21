450db20ee0727445209d66c503a8c9d19cfa1a50
YuliaMeoow <ring.yuliya@gmail.com>
2019-08-21 14:54:33 +0300

    add scroll

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,22 @@
-import React from "react"
+import React from 'react'
 import { 
     Navbar, Nav, NavbarToggler, Collapse,
-    NavbarBrand, NavLink, NavItem, DropdownMenu, DropdownItem, DropdownToggle, UncontrolledDropdown 
+    NavbarBrand, NavLink, NavItem
 } from 'reactstrap'
-
+import { scroller, animateScroll } from 'react-scroll';
 import './styles/header.css'
+
+function scroll(name) {
+  scroller.scrollTo(name, {
+    duration: 1500,
+    smooth: true,
+    offset: -75,
+  })
+}
 
 export default class Example extends React.Component {
     constructor(props) {
-      super(props);
+    super(props);
   
       this.toggle = this.toggle.bind(this);
       this.state = {
@@ -21,28 +29,38 @@ export default class Example extends React.Component {
       });
     }
     render() {
+      const navs = [
+        {
+          name: 'about',
+          title: 'Обо мне',
+        },
+        {
+          name: 'skills',
+          title: 'Что я умею',
+        },
+        {
+          name: 'qualities',
+          title: 'Личные качества',
+        },
+        {
+          name: 'contacts',
+          title: 'Контакты',
+        },
+      ]
       return (
         <div>
-          <Navbar className="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
-            <NavbarBrand href="/main/">
+          <Navbar className='navbar navbar-expand-md bg-dark navbar-dark fixed-top'>
+            <NavbarBrand onClick={animateScroll.scrollToTop}>
                 <img src={require ('./images/logo2.png')} alt="logo" style={{ width: 50 }}></img>
             </NavbarBrand>
             <NavbarToggler onClick={this.toggle} />
             <Collapse isOpen={this.state.isOpen} navbar>
-              <Nav className="ml-auto" navbar>
-                <NavItem>
-                  <NavLink href="/about/"> Обо мне </NavLink>
-                </NavItem>
-                <UncontrolledDropdown nav inNavbar>
-                  <DropdownToggle nav caret> Что я умею </DropdownToggle>
-                    <DropdownMenu right>
-                      <DropdownItem href="/skills/"> Навыки </DropdownItem>
-                      <DropdownItem href="/qualities/"> Личные качества </DropdownItem>
-                    </DropdownMenu>
-                </UncontrolledDropdown>
-                <NavItem>
-                  <NavLink href="/contacts/"> Контакты </NavLink>
-                </NavItem>
+              <Nav className='ml-auto' navbar>
+                {navs.map((nav, i) => {
+                  return <NavItem key={i}>
+                    <NavLink onClick={() => scroll(nav.name)}>{nav.title}</NavLink>
+                  </NavItem>
+                })}
               </Nav>
             </Collapse>
           </Navbar>
